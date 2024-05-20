f1c85c6b82bbd6a175e69316d1ae47cb8a8a4214
Monique Mendes <155989994+moniquemendess@users.noreply.github.com>
2024-05-20 14:32:55 +0200

    instalaciones iniciales (#1)

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react/dom/client";
+import { GifExpertApp } from "./components/GifExpertApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>
+);

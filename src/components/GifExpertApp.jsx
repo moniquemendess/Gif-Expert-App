314d67dadca21ce295beee4c7d776e7f904621ff
Monique Mendes <moniiquemendes96@gmail.com>
2024-05-20 15:26:32 +0200

    lista de categoria con spread operator

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -3,9 +3,15 @@ import { useState } from "react";
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
+  const onAddCategory = () => {
+    setCategories([...categories, "Super Mario"]); // utilizar el spread (desestructuración) para insertar al arreglo
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+      <input />
+      <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;

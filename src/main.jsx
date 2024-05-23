337a39dc4617b9afe9bb90836995a5ec5d0d8dc8
Monique Mendes <155989994+moniquemendess@users.noreply.github.com>
2024-05-23 11:07:53 +0200

    Implementación del archivo barril (#8)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GifExpertApp } from "./components/GifExpertApp";
+import { GifExpertApp } from "./GifExpertApp";
 import "./styles.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GifExpertApp />

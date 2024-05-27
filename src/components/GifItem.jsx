2a50429979247ed70aefb163c6eb1b330d1ae54d
Monique Mendes <155989994+moniquemendess@users.noreply.github.com>
2024-05-27 10:22:22 +0200

    Testing app (#11)
    * Instalación y configuracion de Jest + React Testing Library
    
    * Testing del componente GifGrid
    
    * testing el componente GifItem test 2 y 3

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,7 +1,8 @@
-export const GifItem = ({ title, url }) => {
+export const GifItem = ({ title, url, id }) => {
   return (
     <div className="card">
       <img src={url} alt={title} />
+      <p>{title}</p>
     </div>
   );
 };

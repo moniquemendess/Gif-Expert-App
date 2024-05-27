503fb2f211c8665d1bf166615dd016d6690ce1d3
Monique Mendes <moniiquemendes96@gmail.com>
2024-05-27 10:23:07 +0200

    testing el componente GifItem test 2 y 3

--- tests/components/GifItem.test.js
+++ tests/components/GifItem.test.js
@@ -1,13 +1,31 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
+// Observaciones: screen.debug(); es un console.log  imprime la representación actual del el DOM
+// test 2: verifica si la url es la miesma que estamos avaluando
+// test 3: verificar se el titulo existe utilizando el tobetruthy
+
 describe("Pruebas en <GifItem/>", () => {
+  const title = "Monalisa";
+
+  const url = "https://monalisatest/monalisa.jpg";
+
   test("Debe de hacer match con el snapshot", () => {
-    const title = "Monalisa";
-
-    const url = "https://monalisatest/monalisa.jpg";
-
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test("debe de mostrar la imagen con el URL y el ALT indicado", () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    // expect( screen.getByRole('img').src ).toBe( url );
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(alt);
+  });
+
+  test("debe de mostrar el titulo en componente ", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
